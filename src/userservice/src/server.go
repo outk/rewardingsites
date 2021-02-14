8e7ff17cebca920d5cbf0d6b5d2d18ae25f133be
Tetsuya Kobayashi <outk1023@gmail.com>
2021-02-14 21:47:50 +0900

    Add server.go

--- src/userservice/src/server.go
+++ src/userservice/src/server.go
@@ -1,34 +1,9 @@
 package main
 
-import (
-	"log"
-	"net"
-
-	"github.com/c-tag/timemachine/backend/reservecom/service"
-	"github.com/c-tag/timemachine/backend/util"
-	"github.com/c-tag/timemachine/backend/util/tx"
-	"google.golang.org/grpc/reflection"
-)
-
 const (
 	port = ":50051"
 )
 
 func main() {
-	listenPort, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalln(err)
-	}
 
-	server := util.GetGRPCServer()
-
-	userservice.RegisterUserServiceServiceServer(server, &service.UserService{})
-	reflection.Register(server)
-
-	go tx.Watch()
-
-	log.Printf("Listening on %v", port)
-	if err = server.Serve(listenPort); err != nil {
-		log.Fatalln(err)
-	}
 }
